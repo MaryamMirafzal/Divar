2b3aad555661a7d5c3901873fc3564f46f2d8003
Maryam Mirafzal/ <maryammirafzal9@gmail.com>
2024-10-19 23:58:30 +0330

    [ADD] Get AccessToken with RefreshToken

---
+++ src/Services/token.js
@@ -0,0 +1,17 @@
+import { getCookie } from "utils/cookie";
+import api from "configs/api";
+
+const getNewToken = async () => {
+    const refreshToken = getCookie("refreshToken")
+    if(!refreshToken) return;
+    try {
+        const response = await api.post("auth/check-refresh-token",{
+            refreshToken,
+        });
+        return {response}
+    } catch (error) {
+        return {error}
+    }
+}
+
+export { getNewToken }
--- src/configs/api.js
+++ src/configs/api.js
@@ -1,5 +1,6 @@
 import axios from "axios"
-import { getCookie } from "utils/cookie"
+import { getNewToken } from "Services/token";
+import { getCookie, setCookie } from "utils/cookie"
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL,
@@ -18,6 +19,23 @@ api.interceptors.request.use((request)=>{
     },
     (error)=>{
         return Promise.reject(error)
+    }
+)
+
+api.interceptors.response.use(
+    (response)=>{
+        return response
+    },
+    async (error)=>{
+        const originalRequest  = error.config;
+        if(error.response.status === 401 && !originalRequest._retry){
+            originalRequest._retry = true
+            const res = await getNewToken()
+            if(!res?.response) return
+            setCookie(res.response.data);
+            console.log(res);
+            return api(originalRequest)
+        }
     }
 )
 export default api
